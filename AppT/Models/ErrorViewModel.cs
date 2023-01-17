7780f1bcefffdae5a8e1c38981579cdf6ea8a4a7
Santiago Buitrago <santibuit@hotmail.com>
2023-01-16 22:49:20 -0500

    Base de la aplicacion

---
+++ AppT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
